7aff0e7d955efd91e5d5a55543af4ad7a4e835d9
takaaki.iida <takiida0605@gmail.com>
2017-03-23 12:28:31 +0900

    first commit

---
+++ src/main/java/com/example/AsyncSleuthTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSleuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSleuthTestApplication.class, args);
+	}
+}
